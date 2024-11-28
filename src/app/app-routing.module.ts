c78c601fb3caa3bb0421bf53fea26d68469c14e5
BenjaminPinosa <benjamin.pinosa@e.rascol.net>
2024-11-28 12:28:28 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { NewBooksComponent } from './new-books/new-books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", component : LoginComponent},
+  {path : "login", component : LoginComponent},
+  {path : "admin", component : AdminTemplateComponent, canActivate : [AuthGuard],
+    children : [
+      {path : "home", component : HomeComponent},
+      {path : "books", component : BooksComponent},
+      {path : "new-books", component : NewBooksComponent}
+    ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,66 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './login/login.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatMenu, MatMenuItem, MatMenuModule, MatMenuTrigger} from "@angular/material/menu";
+import {MatDrawerContainer, MatSidenavModule} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+
+import {MatCard, MatCardHeader, MatCardModule} from "@angular/material/card";
+import {MatDivider, MatDividerModule} from "@angular/material/divider";
+
+import {MatTable, MatTableModule} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort, MatSortHeader} from "@angular/material/sort";
+import {MatFormField, MatInput} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthorizationGuard } from './guards/authorization.guard';
+import { BooksComponent } from './books/books.component';
+import { NewBooksComponent } from './new-books/new-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminTemplateComponent,
+    HomeComponent,
+    BooksComponent,
+    NewBooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListItem,
+    MatNavList,
+    MatCardModule,
+    MatDividerModule,
+    MatTableModule,
+    MatPaginator,
+    MatSort,
+    MatSortHeader,
+    MatInput,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(), AuthGuard, AuthorizationGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
